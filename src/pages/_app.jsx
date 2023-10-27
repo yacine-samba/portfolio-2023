4c47556f455caa78a9e729da7eb45eb698abb5b1
Yacine Samba <ysambamezaache@gmail.com>
2023-10-27 05:53:03 +0200

    Initial commit

---
+++ src/pages/_app.jsx
@@ -0,0 +1,10 @@
+import "@/styles/globals.scss";
+
+export default function App({ Component, pageProps }) {
+    return (
+      <div>
+        <Component {...pageProps} />
+        
+      </div>
+    );
+  }
