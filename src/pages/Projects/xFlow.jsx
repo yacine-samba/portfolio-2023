e184ee7b2a1a3a875d6c7314276a7a1311c03928
Yacine Samba <ysambamezaache@gmail.com>
2025-04-07 13:11:26 +0200

    fix xFlow

--- src/pages/Projects/xFlow.jsx
+++ src/pages/Projects/xFlow.jsx
@@ -11,12 +11,10 @@ const xFlow = () => {
 				tag={[
 					'Next.js',
 					'Typescript',
-					'Scss',
 					'Node.js',
-					'Parse server',
-					'Back4App',
+					'Parse server'
 				]}
-				contributor={['Bastien', 'Belkacem', 'Loïc', 'Martin', 'Yacine Samba']}
+				contributor={['Yacine Samba']}
 				link="https://x-flow-umber.vercel.app/"
 				about="xFlow is a project management application that allows users to create and manage their projects. The application provides a user-friendly interface for creating new projects, viewing project details, and managing project tasks. It uses Next.js for the frontend and Parse server for the backend."
 				aboutMore="The application is designed to be intuitive and easy to use, allowing users to focus on their projects without getting bogged down in complex features. It is built using modern web technologies, including Next.js and Typescript, ensuring a fast and responsive user experience."
